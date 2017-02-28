b2d426e02a8d31a12f39f56e7e3b6aafc703f179
katemoksina <katemoksina@gmail.com>
2017-02-28 15:28:25 +0000

    added detailsActivity and modified table design

---
+++ app/src/main/java/com/example/kate/table/DetailsActivity.java
@@ -0,0 +1,17 @@
+package com.example.kate.table;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kate on 28/02/2017.
+ */
+
+public class DetailsActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
