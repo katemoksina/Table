24d4b24743c097ca013cd33b5f8d0aa9cbdc0a95
katemoksina <katemoksina@gmail.com>
2017-03-12 15:15:43 +0000

    sends and receives data from server running on laptop (over wifi network); TODO: table conditional formatting; reading user input; displaying correct attributes in Details screen from DB

--- app/src/main/java/com/example/kate/table/BookingActivity.java
+++ app/src/main/java/com/example/kate/table/BookingActivity.java
@@ -1,20 +1,37 @@
 package com.example.kate.table;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by kate on 28/02/2017.
  */
 
 public class BookingActivity extends Activity{
+
+    private TextView mSent;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setSystemUiVisibility(
@@ -25,20 +42,108 @@ public class BookingActivity extends Activity{
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE);
         setContentView(R.layout.activity_booking);
+
+        mSent = (TextView) findViewById(R.id.sent);
     }
 
     public void submitRecord(View view){
-        JSONObject newRecord = new JSONObject();
-        try {
-            newRecord.put("key", "tu1");
-            newRecord.put("name", "john doe");
-            newRecord.put("email", "jd111");
+//        JSONObject newRecord = new JSONObject();
+//        try {
+//            newRecord.put("key", "tu1");
+//            newRecord.put("name", "john doe");
+//            newRecord.put("email", "jd111");
+//
+//        } catch (JSONException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        String jsonStr = newRecord.toString();
+//        System.out.println("jsonString: "+jsonStr);
 
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        new PostDataTask().execute("http://192.168.0.17:3000/api/booking");
+
+    }
+
+    class PostDataTask extends AsyncTask<String, Void, String>{
+
+
+        ProgressDialog mProgressDialog;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(BookingActivity.this);
+            mProgressDialog.setMessage("Loading data...");
+            mProgressDialog.show();
         }
-        String jsonStr = newRecord.toString();
-        System.out.println("jsonString: "+jsonStr);
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                return postData(params[0]);
+            } catch (IOException e){
+                return "Network error!";
+            } catch (JSONException js){
+                return "Data invalid";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            mSent.setText(result);
+
+            //remove progrss dialog
+            if(mProgressDialog != null){
+                mProgressDialog.dismiss();
+            }
+        }
+
+        private String postData(String urlPath) throws IOException, JSONException{
+
+            StringBuilder result = new StringBuilder();
+            BufferedWriter bufferedWriter = null;
+            BufferedReader bufferedReader = null;
+
+            try {
+            //create data
+            JSONObject dataToSend = new JSONObject();
+            dataToSend.put("name", "Don Joe");
+            dataToSend.put("title", "lecture");
+            dataToSend.put("email", "dj111");
+
+                //initialise request + connect to server
+                URL url = new URL(urlPath);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true); //enable body data
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.connect();
+
+                //write data into server
+                OutputStream outputStream = urlConnection.getOutputStream();
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+                bufferedWriter.write(dataToSend.toString());
+                bufferedWriter.flush();
+
+                //read the response from server
+                InputStream inputStream = urlConnection.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    result.append(line).append("\n");
+                }
+            } finally {
+                if (bufferedReader != null){
+                    bufferedReader.close();
+                }
+                if (bufferedWriter != null){
+                    bufferedWriter.close();
+                }
+            }
+
+            return result.toString();
+        }
     }
 }
